fba46896c8f428b8342b628c1fd01dbba334b57e
TelloViz <lollisjosh@csu.fullerton.edu>
2025-04-16 17:36:58 -0700

    Init Commit

---
+++ StateMachine/IStateMachine.cs
@@ -0,0 +1,17 @@
+﻿namespace Vast.StateMachine {
+    public interface IStateMachine {
+        State AddState(State stateToAdd);
+        State[] AddStates(params State[] statesToAdd);
+        void RemState(State stateToRemove);
+        void RemState(string stateNameToRemove);
+        void RemStates(params State[] statesToRemove);
+        void RemStates(params string[] stateNamesToRemove);
+        void ChangeState(State toState);
+        void ChangeState(string toStateName);
+        void UpdateActiveState();
+        void FixedUpdateActiveState();
+        bool ContainsState(State state);
+        bool ContainsState(string stateName);
+        bool ContainsState(string stateName, out State foundState);
+    }
+}
